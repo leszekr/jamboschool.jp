2b5fa46b2614076f3d282a1635f24be225675468
Leszek <leszek@rybicki.cc>
2012-10-03 19:31:18 +0900

    small css fix

--- public/flipcards.js
+++ public/flipcards.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
 	$('.flip-button').click(function(){
 		$(this).parents('.flipcard').toggleClass('flip');
 		return false;
+	});
+	$(".flipcard").each(function(){
+		$(this).css("height",$(this).find(".front").height());
 	})
 })
